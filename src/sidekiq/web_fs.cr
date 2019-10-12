5d74ff771c6ebb838ecde18140fba1069e3e3e06
Taylor Finnell <tmfinnell@gmail.com>
2019-10-12 11:40:33 -0400

    hack 2

--- src/sidekiq/web_fs.cr
+++ src/sidekiq/web_fs.cr
@@ -9,14 +9,8 @@ module Sidekiq
       resp = ctx.response
       resp.status_code = 200
       resp.content_type = file.mime_type
-      if req.headers["Accept-Encoding"]? =~ /gzip/
-        resp.headers["Content-Encoding"] = "gzip"
-        resp.content_length = file.compressed_size
-        file.write_to_io(resp, compressed: true)
-      else
-        resp.content_length = file.size
-        file.write_to_io(resp, compressed: false)
-      end
+      resp.content_length = file.size
+      file.write_to_io(resp, compressed: false)
     end
   end
 end
